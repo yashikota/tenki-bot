df393b817f0d5145fe39aed60c3c97f591d39a68
kota <52403688+yashikota@users.noreply.github.com>
2022-06-25 16:55:45 +0900

    天気の取得

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import screenshot as ss
-import twitter
+import twitter as tw
 
 
 def main():
     img_path: str = ss.Screenshot().main()
     print("image path: {}".format(img_path))
-    twitter.tweet(img_path)
+    tw.Twitter().tweet(img_path)
 
 
 if __name__ == "__main__":
--- twitter.py
+++ twitter.py
@@ -1,6 +1,8 @@
 import datetime
+import json
 import os
 
+import requests
 import tweepy
 
 
@@ -9,17 +11,26 @@ class Twitter:
         self.token: tweepy = None
         self.api: tweepy = None
         self.text: tweepy = None
+        self.weather: str = ""
 
     def auth(self) -> None:
         self.token.set_access_token(os.environ["AT"], os.environ["AS"])
         self.token = tweepy.OAuthHandler(os.environ["CK"], os.environ["CS"])
         self.api = tweepy.API(self.token)
 
+    def get_weather(self) -> None:
+        url: str = "https://www.jma.go.jp/bosai/forecast/data/forecast/270000.json"
+        res: str = requests.get(url).text
+        data: list = json.loads(res)
+        timeseries: list = data[0]["timeseries"]
+        areas: list = timeseries[0]["areas"]
+        self.weather = areas[0]["weathers"][0]
+
     def gen_text(self) -> None:
         self.text = "{}月{}日の天気は{}です。".format(
             datetime.datetime.now().month,
             datetime.datetime.now().day,
-            "晴れ",
+            self.weather,
         )
 
     def tweet(self, img_path: str) -> None:
